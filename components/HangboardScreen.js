d35c60e74175b7bd127cb5606f9c0c9473494547
Daan van Hasselt <contact@daanvanhasselt.com>
2020-03-22 01:33:26 +0100

    cleanup

--- components/HangboardScreen.js
+++ components/HangboardScreen.js
@@ -8,21 +8,16 @@ import WorkoutStep from './WorkoutStep'
 
 // navigation stack
 const Stack = createStackNavigator()
+const horizontalSlide = { cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS }
 
 function HangboardScreen(props) {
     return (
         <>
             <Header title="Hangboard Training" backButton={false} navigation={props.navigation} />
             <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Workouts" component={WorkoutList} options={{
-                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS ,
-                }}/>
-                <Stack.Screen name="Workout" component={WorkoutDetail} options={{
-                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS ,
-                }}/>
-                <Stack.Screen name="Step" component={WorkoutStep} options={{
-                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS ,
-                }}/>
+                <Stack.Screen name="Workouts" component={WorkoutList} options={horizontalSlide}/>
+                <Stack.Screen name="Workout" component={WorkoutDetail} options={horizontalSlide}/>
+                <Stack.Screen name="Step" component={WorkoutStep} options={horizontalSlide}/>
             </Stack.Navigator>
         </>
     )
